b6d09e98ed8981597c17cd06b1994bbe51db1742
Octavio Armenta <armenta.octavio@outlook.com>
2017-05-06 11:42:34 -0700

    Almost finished SignUpInActivity

---
+++ Traskilada/app/src/main/java/com/example/emiliano/traskilada/views/activities/SignUpInActivity.java
@@ -0,0 +1,19 @@
+package com.example.emiliano.traskilada.views.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.emiliano.traskilada.R;
+
+/**
+ * Created by Octavio on 2017/05/06.
+ */
+
+public class SignUpInActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_in_layout);
+    }
+}
