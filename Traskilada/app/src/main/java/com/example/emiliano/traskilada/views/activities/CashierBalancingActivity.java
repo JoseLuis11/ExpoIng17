72d99a0a047aa34341c2471af376269b9e94d2ad
Octavio Armenta <armenta.octavio@outlook.com>
2017-05-06 05:31:20 -0700

    Almost done Cashier Balancing Activity

--- Traskilada/app/src/main/java/com/example/emiliano/traskilada/views/activities/CashierBalancingActivity.java
+++ Traskilada/app/src/main/java/com/example/emiliano/traskilada/views/activities/CashierBalancingActivity.java
@@ -1,82 +1,48 @@
 package com.example.emiliano.traskilada.views.activities;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.example.emiliano.traskilada.R;
 
 import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 /**
  * Created by Octavio on 2017/05/06.
  */
 
 public class CashierBalancingActivity extends AppCompatActivity {
-    private EditText editText;
-    Calendar myCalendar = Calendar.getInstance();
 
-    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth) {
-            // TODO Auto-generated method stub
-            myCalendar.set(Calendar.YEAR, year);
-            myCalendar.set(Calendar.MONTH, monthOfYear);
-            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            updateLabel();
-        }
-
-    };
-
-    private void updateLabel() {
-
-        String myFormat = "dd/MM/yy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-        editText.setText(sdf.format(myCalendar.getTime()));
-    }
-
+    // UI references
+    private EditText dateEditText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cashier_balancing_activity);
 
+        findViewsById();
+        setCurrencySymbol();
+    }
 
-        editText = (EditText) findViewById(R.id.input_payment_amount);
+    private void findViewsById() {
+        dateEditText = (EditText) findViewById(R.id.input_payment_amount);
+    }
 
-        editText.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(CashierBalancingActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-
-        editText.addTextChangedListener(new TextWatcher() {
+    /***
+     * This method adds the currency symbol at the start of the number in the payment edit text.
+     */
+    private void setCurrencySymbol(){
+        dateEditText.addTextChangedListener(new TextWatcher() {
             private String current = "";
-
-            /***
-             *  This method adds the currency symbol at the start of the number in the payment edit text.
-             */
             @Override
             public void afterTextChanged(Editable s) {
                 if (!s.toString().equals(current)) {
-                    editText.removeTextChangedListener(this);
+                    dateEditText.removeTextChangedListener(this);
                 }
                 String cleanString = s.toString().replaceAll(getString(R.string.symbol_money_local), "");
 
@@ -86,9 +52,9 @@ public class CashierBalancingActivity extends AppCompatActivity {
 
                 current = formatted;
 
-                editText.setText(formatted);
-                editText.setSelection(formatted.length());
-                editText.addTextChangedListener(this);
+                dateEditText.setText(formatted);
+                dateEditText.setSelection(formatted.length());
+                dateEditText.addTextChangedListener(this);
             }
 
             @Override
@@ -99,5 +65,6 @@ public class CashierBalancingActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
         });
+
     }
 }
